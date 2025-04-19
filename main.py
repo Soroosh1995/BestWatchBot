1369cd7406c1af03a94261a3bf039223e0571526
Soroosh1995 <sk.mo74@gmail.com>
2025-04-19 04:33:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from aiohttp import web
 import re
 from datetime import datetime, time
-
+pip install --upgrade pip
 # --- تنظیمات اولیه ---
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
